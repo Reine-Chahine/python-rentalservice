9d8cb18f3130aff1524eab0e7d2c8c81a4a75095
Reine-Chahine <reinechahine3@gmail.com>
2025-03-01 21:19:28 +0200

    creating a init method

--- test.py
+++ test.py
@@ -27,3 +27,6 @@ class capacity(user):
         print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self._rental_price_per_day}/day")
 
 class engine(user):
+      def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
+        super().__init__(brand, model, year, rental_price_per_day)
+        self.engine_capacity = engine_capacity
