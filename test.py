d22488e1d625fd184ddc4b1350b10ac99c3e3f42
Reine-Chahine <reinechahine3@gmail.com>
2025-03-01 21:08:04 +0200

    creating the display info method

--- test.py
+++ test.py
@@ -23,3 +23,5 @@ class capacity(user):
     def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
         super().__init__(brand, model, year, rental_price_per_day)
         self.seating_capacity = seating_capacity
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self._rental_price_per_day}/day")
